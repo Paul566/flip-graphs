d3de840ac50470002f40a9dbe7aa49fed69704e4
Paul566 <arkhipov.pa@phystech.edu>
2023-10-08 19:15:14 +0200

    add flip graph bfs

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <unordered_set>
 #include <fstream>
+#include <unordered_map>
+#include <deque>
 
 #include "Triangulation.h"
 #include "Triangle.h"
 #include "Point.h"
+
 
 Triangulation ReadInitialTriangulation(const std::string& path) {
     std::unordered_set<Triangle, HashTriangle> triangles;
@@ -23,13 +26,36 @@ Triangulation ReadInitialTriangulation(const std::string& path) {
     return Triangulation(triangles);
 }
 
+std::vector<std::pair<int, int>> FlipGraphEdgeList(Triangulation& initial_triangulation) {
+    std::vector<std::pair<int, int>> edge_list;
+    std::unordered_map<Triangulation, int, HashTriangulation> vertex_numbers;
+    vertex_numbers[initial_triangulation] = 0;
+    std::deque<Triangulation> queue;
+    queue.push_back(initial_triangulation);
+    while(!queue.empty()) {
+        Triangulation current_vertex = queue.front();
+        queue.pop_front();
+        auto children = current_vertex.GetChildren();
+        for (const auto& child : children) {
+            if (vertex_numbers.find(child) == vertex_numbers.end()) {
+                edge_list.emplace_back(vertex_numbers.size(), vertex_numbers[current_vertex]);
+                vertex_numbers[child] = static_cast<int>(vertex_numbers.size());
+                queue.push_back(child);
+            } else {
+                edge_list.emplace_back(vertex_numbers[child], vertex_numbers[current_vertex]);
+            }
+        }
+    }
+    return edge_list;
+}
 
 int main() {
-    auto tr = ReadInitialTriangulation("/home/paul/Documents/Experiments/flip-graphs/instances/convex-5/initial-triangulation");
+    auto initial_triangulation = ReadInitialTriangulation("/home/paul/Documents/Experiments/flip-graphs/instances/convex-5/initial-triangulation");
 
-    auto children = tr.GetChildren();
-
-    std::cout << children.size() << "\n";
+    auto el = FlipGraphEdgeList(initial_triangulation);
+    for (auto p : el) {
+        std::cout << p.first << " " << p.second << "\n";
+    }
 
     return 0;
 }
